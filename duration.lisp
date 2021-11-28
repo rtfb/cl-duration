bd64f22959c448555b8a1d260e6255a09f0fa7c3
Vytautas Šaltenis <vytas@rtfb.lt>
2021-11-28 22:04:48 +0200

    First stab at the parser
    Still needs macroisation and tests, but seems to work:
    
        * (parse "10m20s")
        #<DURATION 10m20s>

--- duration.lisp
+++ duration.lisp
@@ -3,6 +3,7 @@
 (defclass durslot ()
   ((value
     :initarg :value
+    :accessor value
     :initform nil)
    (units
     :initarg :units
@@ -39,6 +40,13 @@
                (list (first spec) 'd))
            slots)))
 
+(defun-macro-helper slot-accessors (slots)
+  (loop for slot in slots
+        for slotname = (first slot)
+        for func-name = (concatenate 'string "set-" (symbol-name slotname))
+        collect `(defun ,(intern (string-upcase func-name)) (dur new-value)
+                   (setf (value (,slotname dur)) new-value))))
+
 (defmacro define-duration-class (slots)
   `(progn
      (defclass duration ()
@@ -51,7 +59,9 @@
                (mapcar #'(lambda (slot)
                            (when slot
                              (durslot->string slot)))
-                       ,(all-slots slots))))))
+                       ,(all-slots slots))))
+     ,@(slot-accessors slots)
+     ))
 
 (define-duration-class
   ((weeks "w")
@@ -73,3 +83,55 @@
                      (hours "h")
                      (minutes "m")
                      (seconds "s")))))
+
+(defun extract-num-and-suffix (input start)
+  (multiple-value-bind (num end-num)
+    (parse-integer input :junk-allowed t :start start)
+    (let* ((next-num (loop for i from (1+ end-num) to (1- (length input))
+                           for ch = (char input i)
+                           while (alpha-char-p ch)
+                           finally (return i)))
+           (suffix (subseq input end-num next-num)))
+      (values num suffix next-num))))
+
+(defun parse-parts (input)
+  (let (duration (make-duration))
+    (loop with index = 0 and num = 0 and suffix = ""
+          do (setf (values num suffix index)
+                   (extract-num-and-suffix input index))
+          collect (list num suffix index)
+          ; do (format t "~a ~a ~a~$" num suffix index)
+          ; do (funcall (accessor-by-suffix suffix) duration num))))
+          while (< index (length input))
+          )))
+
+(defun accessor-by-suffix (suffix)
+  (cond
+    ((string= suffix "w")
+     'set-weeks)
+    ((string= suffix "d")
+     'set-days)
+    ((string= suffix "h")
+     'set-hours)
+    ((string= suffix "m")
+     'set-minutes)
+    ((string= suffix "s")
+     'set-seconds)
+    ((string= suffix "ms")
+     'set-millis)
+    ((string= suffix "μs")
+     'set-micros)
+    ((string= suffix "ns")
+     'set-nanos)))
+
+(defun apply-duration-part (duration partspec)
+  (let ((num (first partspec))
+        (suffix (second partspec)))
+    (funcall (accessor-by-suffix suffix) duration num)))
+
+(defun parse (input)
+  (let ((dur (make-duration)))
+    (mapcar #'(lambda (partspec)
+                (apply-duration-part dur partspec))
+            (parse-parts input))
+    dur))
